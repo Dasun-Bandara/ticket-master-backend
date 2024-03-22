f230ccfa187c18c64fe630a523f233ab719b3a40
Dasun Bandara <dasun8517@gmail.com>
2024-03-22 14:32:06 +0530

    Add controllers

---
+++ src/main/java/lk/ijse/dep11/app/api/TicketChargeController.java
@@ -0,0 +1,9 @@
+package lk.ijse.dep11.app.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/chargers")
+public class TicketChargeController {
+}
